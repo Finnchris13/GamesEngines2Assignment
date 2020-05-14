3adfb9b57ab02317681f0ad086b18ee9f02cd740
Finnchris13 <finnchris13@gmail.com>
2020-05-14 17:09:11 +0100

    Set up video controls

---
+++ GamesEngines2AssignmentC17478622/Assets/Scripts/VideoControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoControl : MonoBehaviour
+{
+
+    public GameObject VideoController;
+
+    public float transparentTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var videoPlayer = VideoController.GetComponent<UnityEngine.Video.VideoPlayer>();
+        Invoke("setSeeThrough", transparentTimer);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void setSeeThrough()
+    {
+        var videoPlayer = VideoController.GetComponent<UnityEngine.Video.VideoPlayer>();
+        videoPlayer.targetCameraAlpha = 0;
+    }
+
+}
